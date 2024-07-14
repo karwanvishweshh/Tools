ca3b036616e0ec4329b0e5576960e1bd831eec93
Karwan Vishweshwar <krwnvishu@gmail.com>
2024-07-14 17:45:53 +0530

    TL-created payment branchand added upi payment class

---
+++ BootDatajpaProj01-crudRepository/src/main/java/com/nt/payment/UPI_Payment.java
@@ -0,0 +1,9 @@
+package com.nt.payment;
+
+public class UPI_Payment {
+	public String doPayment(long phoneno,double amount) {
+		System.out.println("payment is done");
+		return "Payment is done";
+	}
+
+}
